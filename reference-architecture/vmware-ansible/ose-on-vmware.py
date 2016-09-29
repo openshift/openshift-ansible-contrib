9eedb2633b01f0fbb51833f0a2b981a35af27e3d
Davis Phillips <davis.phillips@gmail.com>
2016-09-29 11:52:07 -0500

    Adding dynamic inventory generation for VMs and IP addresses to wrapper script

--- reference-architecture/vmware-ansible/ose-on-vmware.py
+++ reference-architecture/vmware-ansible/ose-on-vmware.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # vim: sw=2 ts=2
 
-import click, os, sys, fileinput
+import click, os, sys, fileinput, json, iptools
 
 @click.command()
 
@@ -53,12 +53,12 @@ import click, os, sys, fileinput
 ### Miscellaneous options
 @click.option('--byo_lb', default='no', help='skip haproxy install when one exists within the environment',
               show_default=True)
-@click.option('--lb_fqdn', default='haproxy-0.vcenter.e2e.bos.redhat.com', help='Used for OpenShift cluster hostname and public hostname',
+@click.option('--lb_fqdn', default='haproxy-0', help='Used for OpenShift cluster hostname and public hostname',
               show_default=True)
 
 @click.option('--byo_nfs', default='no', help='skip nfs install when one exists within the environment',
               show_default=True)
-@click.option('--nfs_registry_host', default='nfs-0.vcenter.e2e.bos.redhat.com', help='NFS server for persistent registry',
+@click.option('--nfs_registry_host', default='nfs-0', help='NFS server for persistent registry',
               show_default=True)
 @click.option('--nfs_registry_mountpoint', default='/registry', help='NFS share for persistent registry',
               show_default=True)
@@ -69,6 +69,13 @@ import click, os, sys, fileinput
 @click.option('-v', '--verbose', count=True)
 @click.option('-t', '--tag', help='Ansible playbook tag for specific parts of playbook')
 @click.option('-l', '--local', is_flag=True,help='Local installation of ansible instead of our container')
+# Create inventory options
+@click.option('--create_inventory', is_flag=True, help='Helper script to create json inventory file and exit')
+@click.option('--master_nodes', default='3', help='Number of master nodes to create')
+@click.option('--infra_nodes', default='2', help='Number of infra nodes to create')
+@click.option('--app_nodes', default='3', help='Number of app nodes to create')
+@click.option('--vm_ipaddr_start', default='10.19.114.224', help='Starting IP address to use')
+@click.option('--ose_hostname_prefix', default=None, help='A prefix for your VM guestnames and DNS names: e.g. ose3-')
 
 def launch_refarch_env(console_port=8443,
                     deployment_type=None,
@@ -76,7 +83,7 @@ def launch_refarch_env(console_port=8443,
                     vcenter_username=None,
                     vcenter_password=None,
                     vcenter_template_name=None,
-		            vcenter_folder=None,
+		    vcenter_folder=None,
                     vcenter_datacenter=None,
                     vcenter_cluster=None,
                     vcenter_datastore=None,
@@ -97,9 +104,15 @@ def launch_refarch_env(console_port=8443,
                     nfs_registry_host=None,
                     nfs_registry_mountpoint=None,
                     no_confirm=False,
-		            tag=None,
+		    tag=None,
                     verbose=0,
-		    local=None):
+		    local=None,
+		    create_inventory=None,
+		    master_nodes=None,
+		    infra_nodes=None,
+		    app_nodes=None,
+		    vm_ipaddr_start=None,
+		    ose_hostname_prefix=None):
 
   # Need to prompt for the R53 zone:
   if public_hosted_zone is None:
@@ -121,9 +134,13 @@ def launch_refarch_env(console_port=8443,
   wildcard_zone="%s.%s" % (app_dns_prefix, public_hosted_zone)
 
   tags = []
-
+  
+  # Our initial support node is the wildcard_ip
+  support_nodes=1
   if byo_nfs == "no":
-      nfs_registry_host = 'nfs-0.vcenter.e2e.bos.redhat.com'
+      support_nodes=support_nodes+1
+      nfs_host = nfs_registry_host
+      nfs_registry_host = nfs_host + '.' + public_hosted_zone
       nfs_registry_mountpoint ='/registry'
       tags.append('nfs')
   else:
@@ -133,13 +150,129 @@ def launch_refarch_env(console_port=8443,
   tags.append('prod')
 
   if byo_lb == "no":
-      lb_fqdn = 'haproxy-0.vcenter.e2e.bos.redhat.com'
+      support_nodes=support_nodes+1
+      lb_host = lb_fqdn
+      lb_fqdn = lb_host + '.' + public_hosted_zone
       tags.append('haproxy')
   else:
       lb_fqdn = click.prompt("Please enter the load balancer fqdn for installation:")
 
+  if create_inventory is True:
+  	click.echo('Configured inventory values:')
+	click.echo('\tmaster_nodes: %s' % master_nodes)
+	click.echo('\tinfra_nodes: %s' % infra_nodes)
+  	click.echo('\tapp_nodes: %s' % app_nodes)
+  	click.echo('\tpublic_hosted_zone: %s' % public_hosted_zone)
+  	click.echo('\tapp_dns_prefix: %s' % app_dns_prefix)
+  	click.echo('\tbyo_nfs: %s' % byo_nfs)
+	if byo_nfs == "no":
+  		click.echo('\tNFS VM name: %s' % nfs_host)
+	  	click.echo('\tbyo_lb: %s' % lb_host)
+	if byo_lb == "no":
+  		click.echo('\tHAproxy LB VM name: %s' % lb_host)
+	  	click.echo('\tStarting IP: %s' % vm_ipaddr_start)
+	
+	# Create the inventory file and exit 
+	total_nodes=int(master_nodes)+int(app_nodes)+int(infra_nodes)+int(support_nodes)
 
+	if vm_ipaddr_start is None:
+    		vm_ipaddr_start = click.prompt("Starting IP address to use?")
+	
+	ip4addr = []
+	for i in range(total_nodes):
+	        p = iptools.ipv4.ip2long(vm_ipaddr_start) + i
+		ip4addr.append(iptools.ipv4.long2ip(p))
+	wild_ip =  ip4addr.pop()
 
+	bind_entry = []
+	bind_entry.append("$ORIGIN " + app_dns_prefix + "." + public_hosted_zone + ".")
+	bind_entry.append("*    A       " + wild_ip)
+	bind_entry.append("$ORIGIN " + public_hosted_zone + ".")
+
+	d = {}
+	d['host_inventory'] = {}
+	d['infrastructure_hosts'] = {}	
+
+	support_list = []
+	if byo_nfs == "no":
+		d['host_inventory'][nfs_host] = {}
+        	d['host_inventory'][nfs_host]['guestname'] = nfs_host
+		d['host_inventory'][nfs_host]['ip4addr'] = ip4addr[0]
+	        d['host_inventory'][nfs_host]['tag'] = "nfs"
+        	d['infrastructure_hosts']["nfs_server"] = {}
+	        d['infrastructure_hosts']["nfs_server"]['guestname'] = nfs_host
+        	d['infrastructure_hosts']["nfs_server"]['tag'] = "nfs"
+	        support_list.append(nfs_host)
+        	bind_entry.append(nfs_host + "  A       "       + ip4addr[0])
+	        del ip4addr[0]
+	if byo_lb == "no":
+	        d['host_inventory'][lb_host] = {}
+        	d['host_inventory'][lb_host]['guestname'] = lb_host
+	        d['host_inventory'][lb_host]['ip4addr'] = ip4addr[0]
+       		d['host_inventory'][lb_host]['tag'] = "loadbalancer"
+	        d['infrastructure_hosts']["haproxy"] = {}
+        	d['infrastructure_hosts']["haproxy"]['guestname'] = lb_host
+	        d['infrastructure_hosts']["haproxy"]['tag'] = "loadbalancer"
+        	support_list.append(lb_host)
+	        bind_entry.append(lb_host + "   A       "       + wild_ip)
+
+	master_list = []
+	d['production_hosts'] = {}
+	for i in range(0, int(master_nodes)):
+		if ose_hostname_prefix is not None:
+			master_name=ose_hostname_prefix+"master-"+str(i)
+		else:
+                	master_name="master-"+str(i)
+        	d['host_inventory'][master_name] = {}
+	        d['host_inventory'][master_name]['guestname'] = master_name
+       		d['host_inventory'][master_name]['ip4addr'] = ip4addr[0]
+	        d['host_inventory'][master_name]['tag'] = "master"
+        	d['production_hosts'][master_name] = {}
+	        d['production_hosts'][master_name]['guestname'] = master_name
+	        d['production_hosts'][master_name]['tag'] = "master"
+	        master_list.append(master_name)
+	        bind_entry.append(master_name + "       A       "       + ip4addr[0])
+	        del ip4addr[0]
+	app_list = []
+	for i in range(0, int(app_nodes)):
+        	if ose_hostname_prefix is not None:
+                	app_name=ose_hostname_prefix+"app-"+str(i)
+
+	        else:
+        	        app_name="app-"+str(i)
+
+		d['host_inventory'][app_name] = {}
+	        d['host_inventory'][app_name]['guestname'] = app_name
+       		d['host_inventory'][app_name]['ip4addr'] = ip4addr[0]
+	        d['host_inventory'][app_name]['tag'] = "app"
+       		d['production_hosts'][app_name] = {}
+	        d['production_hosts'][app_name]['guestname'] = app_name
+       		d['production_hosts'][app_name]['tag'] = "app"
+	        app_list.append(app_name)
+        	bind_entry.append(app_name + "  A       "       + ip4addr[0])
+	        del ip4addr[0]
+	infra_list = []
+	for i in range(0, int(infra_nodes)):
+        	if ose_hostname_prefix is not None:
+                	infra_name=ose_hostname_prefix+"infra-"+str(i)
+		else:
+                	infra_name="infra-"+str(i)
+		d['host_inventory'][infra_name] = {}
+	        d['host_inventory'][infra_name]['guestname'] = master_name
+        	d['host_inventory'][infra_name]['ip4addr'] = ip4addr[0]
+	        d['host_inventory'][infra_name]['tag'] = "infra"
+       		d['production_hosts'][infra_name] = {}
+	        d['production_hosts'][infra_name]['guestname'] = infra_name
+       		d['production_hosts'][infra_name]['tag'] = "app"
+	        infra_list.append(infra_name)
+        	bind_entry.append(infra_name + "        A       "       + ip4addr[0])
+	        del ip4addr[0]
+	print "# Here is what should go into your DNS records"
+	print("\n".join(bind_entry))
+
+	with open('infrastructure.json', 'w') as outfile:
+	    json.dump(d, outfile)
+	exit(0)
   # Display information to the user about their choices
   click.echo('Configured values:')
   click.echo('\tconsole port: %s' % console_port)
@@ -184,6 +317,10 @@ def launch_refarch_env(console_port=8443,
 
   if not no_confirm:
     click.confirm('Continue using these values?', abort=True)
+
+  if not os.path.isfile('infrastructure.json'):
+	print "Please create your inventory file first by running the --create_inventory flag"
+  	exit (1)
 
   inventory_file = "inventory/vsphere/vms/vmware_inventory.ini"
   # Add section here to modify inventory file based on input from user check your vmmark scripts for parsing the file and adding the values
