517776f16387c1d83e82f045d7aaf4c5936c1f03
Jason DeTiberus <jdetiber@redhat.com>
2016-11-21 16:02:27 -0500

    update ec2_zones_by_region lookup plugin

--- playbooks/provisioning/aws/cloudformation/lookup_plugins/ec2_zones_by_region.py
+++ playbooks/provisioning/aws/cloudformation/lookup_plugins/ec2_zones_by_region.py
@@ -11,11 +11,43 @@ class LookupModule(LookupBase):
         self.basedir = basedir
 
     def run(self, regions, variables, **kwargs):
+        if isinstance(region, list):
+            region = region[0]
+
+        if not isinstance(region, basestring):
+            raise AnsibleError("type of region is: %s region is: %s" %
+                    (type(region), region))
+
         try:
-            zones = []
-            for region in regions:
-                conn = boto.ec2.connect_to_region(region)
-                zones += [z.name for z in conn.get_all_zones()]
+            conn = boto.ec2.connect_to_region(region)
+            if conn is None:
+                raise AnsibleError("Could not connect to region %s" % region)
+
+            zones = [z.name for z in conn.get_all_zones()]
+            if "us-east-1b" in zones: zones.remove("us-east-1b");
+                return zones
+        except Exception as e:
+            raise AnsibleError("Could not lookup zones for region: %s\nexception: %s" % (region, e))
+
+        try:
+            conn = boto.ec2.connect_to_region(region)
+            if conn is None:
+                raise AnsibleError("Could not connet to region %s" % region)
+            zones = [z.name for z in conn.get_all_zones()]
+            vpc_conn = boto.vpc.connect_to_region(region)
+            vpcs = vpc_conn.get_all_vpcs()
+            default_vpcs = [ v for v in vpcs if v.is_default ]
+
+            # If there are vpc subnets available, then gather list of zones
+            # from zones with subnets. This prevents returning regions that
+            # are not vpc enabled. If the account is an ec2 Classic account
+            # without any VPC subnets, this could result in returning zones
+            # that are not vpc-enabled.
+            subnets = vpc_conn.get_all_subnets()
+            if len(subnets) > 0:
+                subnet_zones = list(set([s.availability_zone for s in subnets]))
+                return subnet_zones
+
             return zones
         except Exception as e:
             raise AnsibleError("Could not lookup zones for region: %s\nexception: %s" % (region, e))
