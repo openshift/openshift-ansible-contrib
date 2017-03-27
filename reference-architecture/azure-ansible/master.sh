8762183784d762e73ae516bd0968b06b453324e2
Glenn West <gwest@redhat.com>
2017-03-27 10:01:36 +0800

    remove docker-storage-setup command, but update file for ansible scripts use

--- reference-architecture/azure-ansible/master.sh
+++ reference-architecture/azure-ansible/master.sh
@@ -47,6 +47,14 @@ yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash
                                                                                          
 touch /root/.updateok
 
+mkdir -p /var/lib/origin/openshift.local.volumes
+ZEROVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | cut -d':' -f1 )
+parted -s -a optimal ${ZEROVG} mklabel gpt -- mkpart primary xfs 1 -1
+sleep 5
+mkfs.xfs -f ${ZEROVG}1
+echo "${ZEROVG}1  /var/lib/origin/openshift.local.volumes xfs  defaults,grpquota  0  0" >> /etc/fstab
+mount ${ZEROVG}1
+
 DOCKERVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | cut -d':' -f1 )
 
 echo "DEVS=${DOCKERVG}" >> /etc/sysconfig/docker-storage-setup
@@ -55,22 +63,7 @@ DEVS=$DOCKERVG
 VG=docker-vg
 EXTRA_DOCKER_STORAGE_OPTIONS="--storage-opt dm.basesize=3G"
 EOF
-docker-storage-setup
-if [ $? -eq 0 ]
-then
-   echo "Docker thin pool logical volume created successfully"
-else
-   echo "Error creating logical volume for Docker"
-   exit 5
-fi
 
-mkdir -p /var/lib/origin/openshift.local.volumes
-ZEROVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | cut -d':' -f1 )
-parted -s -a optimal ${ZEROVG} mklabel gpt -- mkpart primary xfs 1 -1
-sleep 5
-mkfs.xfs -f ${ZEROVG}1
-echo "${ZEROVG}1  /var/lib/origin/openshift.local.volumes xfs  defaults,grpquota  0  0" >> /etc/fstab
-mount ${ZEROVG}1
 
 
 cat <<EOF > /home/${USERNAME}/.ansible.cfg
--- reference-architecture/azure-ansible/node.sh
+++ reference-architecture/azure-ansible/node.sh
@@ -8,12 +8,6 @@ systemctl start dnsmasq.service
 #yum -y update
 yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion
 
-cat <<EOF > /etc/sysconfig/docker-storage-setup
-DEVS=/dev/sdc
-VG=docker-vg
-EXTRA_DOCKER_STORAGE_OPTIONS="--storage-opt dm.basesize=3G"
-EOF
-
 mkdir -p /var/lib/origin/openshift.local.volumes
 ZEROVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | cut -d':' -f1 )
 parted -s -a optimal ${ZEROVG} mklabel gpt -- mkpart primary xfs 1 -1
@@ -21,6 +15,15 @@ sleep 5
 mkfs.xfs -f ${ZEROVG}1
 echo "${ZEROVG}1  /var/lib/origin/openshift.local.volumes xfs  defaults,grpquota  0  0" >> /etc/fstab
 mount ${ZEROVG}1
+
+DOCKERVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | cut -d':' -f1 )
+
+echo "DEVS=${DOCKERVG}" >> /etc/sysconfig/docker-storage-setup
+cat <<EOF > /etc/sysconfig/docker-storage-setup
+DEVS=$DOCKERVG
+VG=docker-vg
+EXTRA_DOCKER_STORAGE_OPTIONS="--storage-opt dm.basesize=3G"
+EOF
 
 touch /root/.updateok
 
