a82bc79777f36e59276fca1a6479673e6464cd13
Glenn West <gwest@redhat.com>
2017-03-28 17:16:24 +0800

    Change to short host names to be compatible with azure pvc

--- reference-architecture/azure-ansible/bastion.sh
+++ reference-architecture/azure-ansible/bastion.sh
@@ -329,23 +329,23 @@ openshift_master_cluster_public_hostname=${RESOURCEGROUP}.trafficmanager.net
 
 
 [masters]
-master1.${domain} openshift_node_labels="{'role': 'master'}"
-master2.${domain} openshift_node_labels="{'role': 'master'}"
-master3.${domain}  openshift_node_labels="{'role': 'master'}"
+master1 openshift_node_labels="{'role': 'master'}"
+master2 openshift_node_labels="{'role': 'master'}"
+master3 openshift_node_labels="{'role': 'master'}"
 
 [etcd]
-master1.${domain}
-master2.${domain}
-master3.${domain}
+master1
+master2
+master3
 
 [nodes]
-master1.${domain} openshift_node_labels="{'role':'master','zone':'default'}"
-master2.${domain} openshift_node_labels="{'role':'master','zone':'default'}"
-master3.${domain} openshift_node_labels="{'role':'master','zone':'default'}"
-node[01:${NODECOUNT}].${domain} openshift_node_labels="{'role': 'app', 'zone': 'default'}"
-infranode1.${domain}  openshift_node_labels="{'role': 'infra', 'zone': 'default'}"
-infranode2.${domain}  openshift_node_labels="{'role': 'infra', 'zone': 'default'}"
-infranode3.${domain} openshift_node_labels="{'role': 'infra', 'zone': 'default'}"
+master1 openshift_node_labels="{'role':'master','zone':'default'}"
+master2 openshift_node_labels="{'role':'master','zone':'default'}"
+master3 openshift_node_labels="{'role':'master','zone':'default'}"
+node[01:${NODECOUNT}] openshift_node_labels="{'role': 'app', 'zone': 'default'}"
+infranode1 openshift_node_labels="{'role': 'infra', 'zone': 'default'}"
+infranode2 openshift_node_labels="{'role': 'infra', 'zone': 'default'}"
+infranode3 openshift_node_labels="{'role': 'infra', 'zone': 'default'}"
 
 EOF
 
