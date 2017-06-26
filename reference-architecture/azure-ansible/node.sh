de49025e89d7a8a69698b42a3a8b6b8c628cc250
Eduardo Minguez Perez <edu@redhat.com>
2017-06-26 18:25:21 +0200

    Set swap size to 0...

--- reference-architecture/azure-ansible/node.sh
+++ reference-architecture/azure-ansible/node.sh
@@ -26,7 +26,8 @@ DATA_SIZE=95%VG
 EXTRA_DOCKER_STORAGE_OPTIONS="--storage-opt dm.basesize=3G"
 EOF
 
-sed -i -e 's/ResourceDisk.EnableSwap=y/ResourceDisk.EnableSwap=n/g' /etc/waagent.conf
+sed -i -e 's/ResourceDisk.EnableSwap.*/ResourceDisk.EnableSwap=n/g' /etc/waagent.conf
+sed -i -e 's/ResourceDisk.SwapSizeMB.*/ResourceDisk.SwapSizeMB=0/g' /etc/waagent.conf
 swapoff -a
 # Do not restart waagent as it will make the installation fail
 #systemctl restart waagent.service
